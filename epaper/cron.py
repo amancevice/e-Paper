e42cb88c17c53c25e559f4dbc5b49b0bf5fa3024
Alexander Mancevice <alexander.mancevice@hey.com>
2020-11-28 20:29:40 -0500

    hour/minutes

--- epaper/cron.py
+++ epaper/cron.py
@@ -13,15 +13,23 @@ from .epaper import (
 
 
 def main():
-    filename = datetime.now().strftime('T%I00.BMP').encode()
+    now = datetime.now()
+    hour = 'H{:02d}.BMP'.format(now.hour % 12).encode()
+    minute = 'M{:02d}.BMP'.format(now.minute - now.minute % 5).encode()
+    y = 315 if hour != b'H08.BMP' else 395
+
     with EPaper() as paper:
         paper.send(Handshake())
         time.sleep(2)
         paper.send(SetCurrentDisplayRotation(SetCurrentDisplayRotation.FLIP))
+
         paper.send(FillRectangle(0, 0, 800, 600))
-        paper.send(DisplayImage(0, 0, filename))
+        paper.send(DisplayImage(0, 0, hour))
+        paper.read_responses()
+
+        paper.send(DisplayImage(0, y, minute))
         paper.send(RefreshAndUpdate())
-        paper.read_responses(timeout=10)
+        paper.read_responses()
 
 
 if __name__ == '__main__':
