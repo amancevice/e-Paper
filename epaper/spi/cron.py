a88031912033cdd09c469f42095da7ad4c92d70c
Alexander Mancevice <alexander.mancevice@hey.com>
2020-12-05 16:34:46 -0500

    SPI updates

--- epaper/spi/cron.py
+++ epaper/spi/cron.py
@@ -8,13 +8,12 @@ logging.basicConfig(level=logging.DEBUG)
 
 
 def main():
+    blk = Image.open('400x300/BH11M21.BMP')
+    red = Image.open('400x300/RH11M21.BMP')
     with EPaper(400, 300) as epd:
         epd.init()
         epd.clear()
-
-        # imageblack = self.getbuffer(Image.open('../pic/4in2b-b.bmp'))
-        # imagered = self.getbuffer(Image.open('../pic/4in2b-r.bmp'))
-        # self.display(imageblack, imagered)
+        epd.display(epd.getbuffer(blk), epd.getbuffer(red))
 
 
 if __name__ == '__main__':
--- waveshare/spi.py
+++ waveshare/spi.py
@@ -7,7 +7,7 @@ from RPi import GPIO
 
 class RaspberryPi:
     # Pin definition
-    RST_PIN = 17
+    RESET_PIN = 17
     DC_PIN = 25
     CS_PIN = 8
     BUSY_PIN = 24
@@ -31,7 +31,7 @@ class RaspberryPi:
     def module_init(self):
         GPIO.setmode(GPIO.BCM)
         GPIO.setwarnings(False)
-        GPIO.setup(self.RST_PIN, GPIO.OUT)
+        GPIO.setup(self.RESET_PIN, GPIO.OUT)
         GPIO.setup(self.DC_PIN, GPIO.OUT)
         GPIO.setup(self.CS_PIN, GPIO.OUT)
         GPIO.setup(self.BUSY_PIN, GPIO.IN)
@@ -44,7 +44,7 @@ class RaspberryPi:
         self.SPI.close()
 
         logging.debug('close 5V, Module enters 0 power consumption ...')
-        GPIO.output(self.RST_PIN, 0)
+        GPIO.output(self.RESET_PIN, 0)
         GPIO.output(self.DC_PIN, 0)
 
         GPIO.cleanup()
@@ -53,73 +53,40 @@ class RaspberryPi:
 class EPaper:
     def __init__(self, width=400, height=300):
         self.board = RaspberryPi()
-        self.reset_pin = self.board.RST_PIN
-        self.dc_pin = self.board.DC_PIN
-        self.busy_pin = self.board.BUSY_PIN
-        self.cs_pin = self.board.CS_PIN
         self.width = width
         self.height = height
+        self.size = int(self.width * self.height / 8)
 
     def __enter__(self):
-        """
-        So the EPaper class can be used in a with clause and
-        handle cleaning up the GPIO stuff on exit.  It returns itself.
-        """
+        self.init()
         return self
 
     def __exit__(self, type, value, traceback):
-        """
-        Invokes the GPIO.cleanup() method.  If that's not a desired behavior,
-        don't use the with clause.
-        """
         self.dev_exit()
         GPIO.cleanup()
 
-    # Hardware reset
-    def reset(self):
-        self.board.digital_write(self.reset_pin, 1)
-        self.board.delay_ms(200)
-        self.board.digital_write(self.reset_pin, 0)
-        self.board.delay_ms(10)
-        self.board.digital_write(self.reset_pin, 1)
-        self.board.delay_ms(200)
+    def clear(self):
+        self.send_command(0x10)
+        self.send_data(*([0xFF] * self.size))
 
-    def send_command(self, command):
-        self.board.digital_write(self.dc_pin, 0)
-        self.board.digital_write(self.cs_pin, 0)
-        self.board.spi_writebyte([command])
-        self.board.digital_write(self.cs_pin, 1)
+        self.send_command(0x13)
+        self.send_data(*([0xFF] * self.size))
 
-    def send_data(self, data):
-        self.board.digital_write(self.dc_pin, 1)
-        self.board.digital_write(self.cs_pin, 0)
-        self.board.spi_writebyte([data])
-        self.board.digital_write(self.cs_pin, 1)
-
-    def read_busy(self):
-        logging.debug("e-Paper busy")
-        while(self.board.digital_read(self.busy_pin) == 0):  # 0: idle, 1: busy
-            self.board.delay_ms(100)
-        logging.debug("e-Paper busy release")
-
-    def init(self):
-        if (self.board.module_init() != 0):
-            return -1
-
-        self.reset()
-
-        self.send_command(0x06)  # BOOSTER_SOFT_START
-        self.send_data(0x17)
-        self.send_data(0x17)
-        self.send_data(0x17)  # 07 0f 17 1f 27 2F 37 2f
-
-        self.send_command(0x04)  # POWER_ON
+        self.send_command(0x12)
         self.read_busy()
 
-        self.send_command(0x00)  # PANEL_SETTING
-        self.send_data(0x0F)  # LUT from OTP
+    def dev_exit(self):
+        self.board.module_exit()
 
-        return 0
+    def display(self, imageblack, imagered):
+        self.send_command(0x10)
+        self.send_data(*imageblack)
+
+        self.send_command(0x13)
+        self.send_data(*imagered)
+
+        self.send_command(0x12)
+        self.read_busy()
 
     def getbuffer(self, image):
         # logging.debug('bufsiz = ', int(self.width / 8) * self.height)
@@ -148,35 +115,57 @@ class EPaper:
                         buf[int(z / 8)] &= ~(0x80 >> (y % 8))
         return buf
 
-    def display(self, imageblack, imagered):
-        self.send_command(0x10)
-        for i in range(0, int(self.width * self.height / 8)):
-            self.send_data(imageblack[i])
+    def init(self):
+        if (self.board.module_init() != 0):
+            return -1
 
-        self.send_command(0x13)
-        for i in range(0, int(self.width * self.height / 8)):
-            self.send_data(imagered[i])
+        self.reset()
 
-        self.send_command(0x12)
+        self.send_command(0x06)  # BOOSTER_SOFT_START
+        self.send_data(0x17)
+        self.send_data(0x17)
+        self.send_data(0x17)  # 07 0f 17 1f 27 2F 37 2f
+
+        self.send_command(0x04)  # POWER_ON
         self.read_busy()
 
-    def clear(self):
-        self.send_command(0x10)
-        for i in range(0, int(self.width * self.height / 8)):
-            self.send_data(0xFF)
+        self.send_command(0x00)  # PANEL_SETTING
+        self.send_data(0x0F)  # LUT from OTP
 
-        self.send_command(0x13)
-        for i in range(0, int(self.width * self.height / 8)):
-            self.send_data(0xFF)
+        return 0
 
-        self.send_command(0x12)
-        self.read_busy()
+    def read_busy(self):
+        logging.debug("e-Paper busy")
+
+        # 0: idle, 1: busy
+        while(self.board.digital_read(self.board.BUSY_PIN) == 0):
+            self.board.delay_ms(100)
+
+        logging.debug("e-Paper busy release")
+
+    def reset(self):
+        self.board.digital_write(self.board.RESET_PIN, 1)
+        self.board.delay_ms(200)
+        self.board.digital_write(self.board.RESET_PIN, 0)
+        self.board.delay_ms(10)
+        self.board.digital_write(self.board.RESET_PIN, 1)
+        self.board.delay_ms(200)
+
+    def send_command(self, command):
+        self.board.digital_write(self.board.DC_PIN, 0)
+        self.board.digital_write(self.board.CS_PIN, 0)
+        self.board.spi_writebyte([command])
+        self.board.digital_write(self.board.CS_PIN, 1)
+
+    def send_data(self, *data):
+        for byte in data:
+            self.board.digital_write(self.board.DC_PIN, 1)
+            self.board.digital_write(self.board.CS_PIN, 0)
+            self.board.spi_writebyte([byte])
+            self.board.digital_write(self.board.CS_PIN, 1)
 
     def sleep(self):
         self.send_command(0x02)  # POWER_OFF
         self.read_busy()
         self.send_command(0x07)  # DEEP_SLEEP
         self.send_data(0xA5)  # check code
-
-    def dev_exit(self):
-        self.board.module_exit()
